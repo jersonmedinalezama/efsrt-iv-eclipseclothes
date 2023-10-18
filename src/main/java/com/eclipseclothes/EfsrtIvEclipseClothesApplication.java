985c7d2fab221e8b52b36c92f9e930df8793a220
Jerson Noe Medina Lezama <i202121806@cibertec.edu.pe>
2023-10-18 10:33:49 -0500

    inicio de proyecto

---
+++ src/main/java/com/eclipseclothes/EfsrtIvEclipseClothesApplication.java
@@ -0,0 +1,13 @@
+package com.eclipseclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfsrtIvEclipseClothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfsrtIvEclipseClothesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eclipseclothes/EfsrtIvEclipseClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eclipseclothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfsrtIvEclipseClothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

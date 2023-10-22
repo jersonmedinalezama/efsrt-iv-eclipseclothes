6ebd105de78151adbfa27fdd14ed478541e30996
Jerson Noe Medina Lezama <i202121806@cibertec.edu.pe>
2023-10-22 12:27:20 -0500

    implementación de página de detalles de producto

--- src/main/java/com/eclipseclothes/controller/HomeController.java
+++ src/main/java/com/eclipseclothes/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -80,5 +81,12 @@ public class HomeController {
 		return "home/index";
 	}
 	
+	@GetMapping("/detalles/{id}")
+	public String detallesProducto(@PathVariable int id, Model model) {
+		
+		model.addAttribute("producto", productoService.obtener(id));
+		
+		return "home/detalles";
+	}
 	
 }
